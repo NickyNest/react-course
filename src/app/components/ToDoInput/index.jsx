d7bfe469381352283f8205eb03663997b93ed693
Mykola Nesterenko <Mykola.Nesterenko@exact.com>
2018-01-16 22:31:40 +0200

    Finished ToDoInput

--- src/app/components/ToDoInput/index.jsx
+++ src/app/components/ToDoInput/index.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {func} from 'prop-types';
 
-const ToDoInput = ({onAddItem}) => {
-    const onClick = () => {
-        onAddItem('text');
+class ToDoInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {task: ''};
+    }
+
+    onChange = e => {
+        this.setState({task: e.target.value});
     };
 
-    return (
-        <div>
-            <input type='text' placeholder='add task' />
-            <button onClick={onClick}>Add</button>
-        </div>
-    );
-};
+    onClick = () => {
+        if (!this.state.task) {
+            return;
+        }
+        this.props.onAddItem(this.state.task);
+        this.setState({task: ''});
+    };
+
+    render() {
+        return (
+            <div>
+                <input type='text' placeholder='add task' onChange={e => this.onChange(e)} value={this.state.task} />
+                <button onClick={this.onClick}>Add</button>
+            </div>
+        );
+    }
+}
 
 ToDoInput.propTypes = {
     onAddItem: func.isRequired
