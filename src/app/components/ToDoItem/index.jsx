a43c6723de84782f917bd43ea599e8792edbce28
Mykola Nesterenko <Mykola.Nesterenko@exact.com>
2018-02-04 19:21:48 +0200

    Get data from API

--- src/app/components/ToDoItem/index.jsx
+++ src/app/components/ToDoItem/index.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import {Checkbox} from 'semantic-ui-react';
-import {shape, number, string, bool, func} from 'prop-types';
+import {shape, string, bool, date, func} from 'prop-types';
 import './index.css';
 
-const ToDoItem = ({item, onCheckChange, onRemoveItem}) => (
+const ToDoItem = ({task, onCheckChange, onRemoveItem}) => (
     <div>
-        <Checkbox toggle onChange={() => onCheckChange(item.id)} />
-        <span className={item.isChecked ? 'throw-text' : ''}>{item.text}</span>
-        <button onClick={() => onRemoveItem(item.id)}>X</button>
+        <Checkbox toggle checked={task.completed} onChange={() => onCheckChange(task.id)} />
+        <span className={task.completed ? 'throw-text' : ''}>{task.title} {task.createdDate}</span>
+        <button onClick={() => onRemoveItem(task.id)}>X</button>
     </div>
 );
 
 ToDoItem.propTypes = {
-    item: shape({
-        id: number.isRequired,
-        text: string.isRequired,
-        isChecked: bool.isRequired
+    task: shape({
+        id: string.isRequired,
+        title: string.isRequired,
+        completed: bool.isRequired,
+        createdDate: date
     }).isRequired,
     onCheckChange: func.isRequired,
     onRemoveItem: func.isRequired
--- src/app/containers/ToDoContainer/index.jsx
+++ src/app/containers/ToDoContainer/index.jsx
@@ -2,20 +2,33 @@ import React, {Component} from 'react';
 import ToDoInput from 'components/ToDoInput';
 import ToDoItem from 'components/ToDoItem';
 import {Grid} from 'semantic-ui-react';
+import fetch from 'isomorphic-fetch';
+import faker from 'faker';
 import './index.css';
 
 class ToDoContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: []
+            tasks: []
         };
     }
 
+    componentWillMount() {
+        fetch('http://localhost:60253/api/tasks')
+            .then(response => response.json())
+            .then(data => this.setState({tasks: data}));
+    }
+
     onAddItem = text => {
-        const maxId = this.state.items.length > 0 ? Math.max(...this.state.items.map(item => item.id)) : 0;
-        const newItem = {id: maxId + 1, text, isChecked: false};
-        this.setState({items: [...this.state.items, newItem] });
+        const createdDate = new Date();
+        const newTask = {
+            id: faker.random.uuid(),
+            title: text,
+            completed: false,
+            createdDate: `${createdDate.getDate()}/${createdDate.getMonth() + 1}/${createdDate.getFullYear()}`
+        };
+        this.setState({tasks: [...this.state.tasks, newTask] });
     };
 
     // onCheckChange = id => {
@@ -27,31 +40,31 @@ class ToDoContainer extends Component {
     // };
 
     onCheckChange = id => {
-        const {items} = this.state;
-        const itemIndex = items.findIndex(item => item.id === id);
-        const newItems = [
-            ...items.slice(0, itemIndex),
-            {...items[itemIndex], isChecked: !items[itemIndex].isChecked},
-            ...items.slice(itemIndex + 1, items.length)
+        const {tasks} = this.state;
+        const taskIndex = tasks.findIndex(task => task.id === id);
+        const newTask = [
+            ...tasks.slice(0, taskIndex),
+            {...tasks[taskIndex], completed: !tasks[taskIndex].completed},
+            ...tasks.slice(taskIndex + 1, taskIndex.length)
         ];
-        this.setState({items: newItems});
+        this.setState({tasks: newTask});
     };
 
     onRemoveItem = id => {
-        this.setState({items: this.state.items.filter(item => item.id !== id)});
+        this.setState({tasks: this.state.tasks.filter(task => task.id !== id)});
     };
 
-    mapToDoItemList = items => (
+    mapToDoTaskList = tasks => (
         <div>
             {
-                items.map(item =>
-                    <ToDoItem key={item.id} item={item} onCheckChange={this.onCheckChange} onRemoveItem={this.onRemoveItem} />)
+                tasks.map(task =>
+                    <ToDoItem key={task.id} task={task} onCheckChange={this.onCheckChange} onRemoveItem={this.onRemoveItem} />)
             }
         </div>
     );
 
     render() {
-        const {items} = this.state;
+        const {tasks} = this.state;
         const title = 'Simple To-Do application';
         return (
             <div>
@@ -59,7 +72,7 @@ class ToDoContainer extends Component {
                 <ToDoInput onAddItem={this.onAddItem} />
                 <Grid>
                     <Grid.Column width={5}>
-                        {items.length === 0 ? 'huhЪ' : this.mapToDoItemList(items)}
+                        {tasks.length === 0 ? 'huhЪ' : this.mapToDoTaskList(tasks)}
                     </Grid.Column>
                 </Grid>
             </div>
