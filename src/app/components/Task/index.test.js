1816dc6ee13fc7414fa26d1ceb79bb51fb4b4351
Mykola Nesterenko <Mykola.Nesterenko@exact.com>
2018-04-26 22:06:01 +0300

    Covered Task with test

--- src/app/components/Task/index.test.js
+++ src/app/components/Task/index.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import Task from './index';
 
 test('Task (completed=true) match expected snapshot', () => {
@@ -18,3 +18,27 @@ test('Task (completed=false) match expected snapshot', () => {
         onRemoveTask={() => {}} />)).toMatchSnapshot();
 });
 
+test('Task with click on button should call onRemoveTask with parameter 1', () => {
+    const onRemoveTaskMock = jest.fn();
+    const wrapper = shallow(<Task
+        task={{id: '1', title: 'temp', completed: false, createdDate: new Date(1900, 1, 1)}}
+        onCheckChange={() => {}}
+        onRemoveTask={onRemoveTaskMock} />);
+    wrapper.find('button').simulate('click');
+
+    expect(onRemoveTaskMock.mock.calls.length).toEqual(1);
+    expect(onRemoveTaskMock.mock.calls[0][0]).toEqual('1');
+});
+
+test('Task with toggle checkbox should call onCheckChange with parameters 1, true', () => {
+    const onCheckChangeMock = jest.fn();
+    const wrapper = mount(<Task
+        task={{id: '1', title: 'temp', completed: true, createdDate: new Date(1900, 1, 1)}}
+        onCheckChange={onCheckChangeMock}
+        onRemoveTask={() => {}} />);
+    wrapper.find('Checkbox').simulate('change');
+
+    expect(onCheckChangeMock.mock.calls.length).toEqual(1);
+    expect(onCheckChangeMock.mock.calls[0][0]).toEqual('1');
+    expect(onCheckChangeMock.mock.calls[0][1]).toEqual(false);
+});
--- src/app/components/TaskInput/index.test.js
+++ src/app/components/TaskInput/index.test.js
@@ -1,14 +1,9 @@
 /* eslint-disable react/jsx-filename-extension */
 
 import React from 'react';
-import {shallow, configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
 import TaskInput from './index';
 
-configure({adapter: new Adapter()});
-
-describe('TaskInput', () => {
-    it('TaskInput match expected snapshot', () => {
-        expect(shallow(<TaskInput onAddTask={() => {}} />)).toMatchSnapshot();
-    });
+test('TaskInput match expected snapshot', () => {
+    expect(shallow(<TaskInput onAddTask={() => {}} />)).toMatchSnapshot();
 });
